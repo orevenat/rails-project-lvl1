9a334a9b2ab5a7083214365ed41f3b524af4b900
Ruslan Knyazev <agrry@yandex.ru>
2022-12-11 20:18:35 +0500

    fix hexlet check

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -4,19 +4,30 @@ module HexletCode
   module Tag
     SELF_CLOSING_TAGS = %i[area base input link br hr img].freeze
 
-    def self.build(tag_name, options = {}, &block)
+    def self.build(tag_name, options = {}, &)
       default_options = default_options_for(tag_name.to_sym)
       options.merge!(default_options.reject { |key, _| options.key? key })
 
       opts = options.except(:body)
                     .map { |key, value| "#{key}=\"#{value}\"" }
                     .join(' ')
-                    .then { |it| it.size.positive? ? " #{it}" : it }
 
-      self_closing_tag = HexletCode::Tag::SELF_CLOSING_TAGS.include?(tag_name.to_sym)
-      tag_end = self_closing_tag ? '' : "</#{tag_name}>"
-      body = self_closing_tag ? '' : (yield block if block_given?)
-      "<#{tag_name}#{opts}>#{options[:body]}#{body}#{tag_end}"
+      if HexletCode::Tag::SELF_CLOSING_TAGS.include?(tag_name.to_sym)
+        render_single_tag(tag_name, opts)
+      else
+        render_tag(tag_name, options[:body], opts, &)
+      end
+    end
+
+    def self.render_single_tag(tag_name, options)
+      opts = options.then { |it| it.size.positive? ? " #{it}" : it }
+      "<#{tag_name}#{opts}>"
+    end
+
+    def self.render_tag(tag_name, body, options)
+      opts = options.then { |it| it.size.positive? ? " #{it}" : it }
+      final_body = block_given? ? yield : body.to_s
+      "<#{tag_name}#{opts}>#{final_body}</#{tag_name}>"
     end
 
     def self.default_options_for(tag)
